7f3fe085cc7c721985f1954496f358b7679e3560
Stanislav Poliakov <polyakov.s.de@sberbank.ru>
2024-08-20 10:54:24 +0300

    Initial Commit

---
+++ FinancialDemo/FinancialDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialDemoApp.swift
+//  FinancialDemo
+//
+//  Created by Поляков Станислав Денисович on 20.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinancialDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
